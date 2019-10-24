e2606fa32de169e74a094765bcae0cf001166942
Dennis Dobslaf <dobslaf@citkomm.de>
2019-10-24 11:44:24 +0200

    Beispiel PHPOffice/PHPSpreadsheet

---
+++ public/spreadsheet.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+$sheet->setCellValue('A1', 'Hello World!');
+
+$writer = new Xlsx($spreadsheet);
+$writer->save(__DIR__ . '/../data/hello_world.xlsx');
